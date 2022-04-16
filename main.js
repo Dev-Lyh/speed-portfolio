b2920a76064c358a1f6cb6d251a690ae324d69ee
emilyfelicio03@gmail.com <emilyfelicio03@gmail.com>
2022-04-16 08:35:31 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+/*
+const firstFunction =
+  {
+    message: {
+      data: ['one', 'one2'],
+    }
+  }
+const secondFunction =
+  {
+    message: {
+      data: ['two', 'two2'],
+    }
+  }
+  const thirdFunction =
+  {
+    message: {
+      data: ['three', 'three2'],
+    }
+  }
+
+const arr = [firstFunction, secondFunction, thirdFunction]
+
+const randomArr = () => {
+  let index = Math.floor(Math.random() * arr.length)
+  let dataInd = Math.floor(Math.random() * 2)
+  const james = arr[index].message.data[dataInd]
+  console.log(james)
+  
+}
+
+setInterval(randomArr, 2000)
+*/
